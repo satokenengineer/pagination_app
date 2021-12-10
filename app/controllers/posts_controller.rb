10d5f73381f1a178cdd113b26829baeb16c4dac4
satokenengine <satokenengineer@gmail.com>
2021-12-10 21:45:57 +0900

    Rails応用課題④  投稿一覧実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.page(params[:page]).per(10)
+  end
+  
+  def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.create!(post_params)
+    redirect_to action: :index
+    
+  end 
+
+  private
+
+  def post_params
+    params.permit(:body)
   end
 end
